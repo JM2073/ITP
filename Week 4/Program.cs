1d5e4cc1c352c625635f105be714d5eff70d888d
JM2073 <JM2073@student.aru.ac.uk>
2022-10-20 11:45:29 +0100

    add 2 weeks worth of projects

---
+++ Week 4/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//
+// using System.Threading.Channels;
+//
+// int[] rainfall = new int[12];
+// Console.WriteLine("Please input 12 rainfall values in mm for a country");
+//
+//
+// for (int i = 1; i <= 12; i++)
+// {
+//     Console.Write($"{i+1} Input:");
+//     rainfall[i] = Template.CustomMethod.ParseStringToInt(Console.ReadLine());
+// }
+//
+// for (int i = 1; i <= 12 ; i++)
+// {
+//     Console.WriteLine($"the month {i+1} had {rainfall[i]}mm of rain");
+// }
+//
+// int avarge= 0;
+// int total = 0;
+// foreach (var i in rainfall)
+// {
+//     total += i;
+// }
+// avarge = total / rainfall.Length;
+// Console.WriteLine($"the avarage of rainfall is {avarge}");
+
+// Console.WriteLine($"the avarage of rainfall is {rainfall.Average()}");
+// string[,] addressBook = new string[3, 2];
+// for (int i = 0; i < 3; i++)
+// {
+//     Console.Write("Enter Name ");
+//     addressBook[i, 0] = Console.ReadLine() ?? " " ;
+//     Console.Write("Enter Address ");
+//     addressBook[i, 1] = Console.ReadLine() ?? " " ;
+// }
+//
+// Console.WriteLine("name , Address");
+// Console.WriteLine("====================");
+//
+// for (var i = 0; i < 3; i++)
+// {
+//     Console.Write($"{addressBook[i,0]} , ");
+//     Console.Write($"{addressBook[i,1]}");
+//     Console.WriteLine();
+// }
+
+using System.Data;
+
+
+Console.ReadKey();
---
+++ Week 5/Program.cs
@@ -0,0 +1,152 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Welcome to the number sorter");
+// int[] arr = new int[5];
+//
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     Console.Write("enter a number :");
+//     arr[i] = Convert.ToInt32(Console.ReadLine());
+// }
+//
+//
+// Console.WriteLine("orignal order");
+// for (int i = 0; i < arr.Length; i++)
+//     Console.Write(arr[i] + " ");
+//
+//
+// int temp = 0;
+//
+// for (int write = 0; write < arr.Length; write++) {
+//     for (int sort = 0; sort < arr.Length - 1; sort++) {
+//         if (arr[sort] > arr[sort + 1]) {
+//             temp = arr[sort + 1];
+//             arr[sort + 1] = arr[sort];
+//             arr[sort] = temp;
+//         }
+//     }
+// }
+//
+// Console.WriteLine("sorted order");
+// for (int i = 0; i < arr.Length; i++)
+//     Console.Write(arr[i] + " ");
+//
+// Console.WriteLine();
+// Console.WriteLine($"your highest number is: {arr[4]}");
+// Console.WriteLine($"your lowest number is: {arr[0]}");
+//
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("welcome to the shiitake mushroom show ");
+//
+// void message()
+// {
+//     Console.WriteLine("hello world");    
+// }
+//
+// double valvArea(int raidus)
+// {
+//     return Math.PI * (raidus * raidus);
+// }
+//
+// double valCurc(int raidus)
+// {
+//     return 2 * Math.PI  * raidus;
+// }
+//
+// message();
+// message();
+// message();
+//
+// Console.WriteLine("please enter raidus.");
+// int raidus = Convert.ToInt32(Console.ReadLine());
+//
+// Console.WriteLine($"the area is {valCurc(raidus)}");
+// Console.WriteLine($"the curm is {valvArea(raidus)}");
+
+bool isNegtive = false;
+
+
+List<int> NumbersIn(int value)
+{
+    if (value == 0) return new List<int>();
+
+    var numbers = NumbersIn(value / 10);
+
+    numbers.Add(value % 10);
+
+    return numbers;
+}
+
+string Letterize(int number)
+{
+    string numText = string.Empty;
+
+    if (number < 0)
+    {
+        isNegtive = true;
+        number *= -1;
+    }
+
+    var numlist = NumbersIn(number);
+
+    numText += isNegtive ? "minus " : " ";
+
+    foreach (var num in numlist)
+    {
+        numText += num switch
+        {
+            1 => " one ",
+            2 => " Two ",
+            3 => " Three ",
+            4 => " fore ",
+            5 => " five ",
+            6 => " six ",
+            7 => " Seven ",
+            8 => " eight ",
+            9 => " nine ",
+            0 => " zero ",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    return numText;
+}
+
+Console.WriteLine("please enter a number between -999 and 999");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+if (choice > 999 || choice < -999)
+    Console.WriteLine($"too {(choice > 999 ? "large" : "small")}");
+else
+    Console.WriteLine(Letterize(choice));
+
+//
+// int number;
+//
+// string Letterize(int number)
+// {
+//     bool isNegtive = false;
+//
+//     if (number < 0)
+//     {
+//         isNegtive = true;
+//         number *= -1;
+//     }
+//
+//     string[] numbers = { "one", "two", "three", "fore", "five", "six", "seven", "eight", "nine", "zero" };
+//
+//     int hundreds = number / 100;
+//     number %= 100;
+//     int tens = number / 10;
+//     number %= 10;
+//
+//     return $"{(isNegtive ? "minus" : "")} {numbers[hundreds - 1]} {numbers[tens - 1]} {numbers[number - 1]}";
+// }
+//
+// Console.WriteLine("please enter a number between -999 and 999");
+// number = Convert.ToInt32(Console.ReadLine());
+//
+// Console.WriteLine($"Number is {Letterize(number)}");
+
+Console.ReadKey();
